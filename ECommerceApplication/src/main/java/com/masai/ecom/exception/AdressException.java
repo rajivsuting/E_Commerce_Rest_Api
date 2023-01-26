f07ca0b66dfe504d0be90a75f03a0cc258e549ee
rajivsuting <rajivsuting16@gmail.com>
2023-01-26 18:22:31 +0530

    update

---
+++ ECommerceApplication/src/main/java/com/masai/ecom/exception/AdressException.java
@@ -0,0 +1,12 @@
+package com.masai.ecom.exception;
+
+public class AdressException extends Exception {
+	
+
+	private static final long serialVersionUID = 1L;
+
+	public AdressException(String message) {
+		super(message);
+	}
+
+}
